2c1e5e2781d54974e7460a0b4e078e5a66de8a67
Denis Alves Gugia <denis.gugia@gmail.com>
2026-04-19 16:36:07 -0400

    fix: adicionar rewrite para assets do onboarding

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         source: "/onboarding/:path*",
         destination: "https://integr8-634054956771.us-central1.run.app/:path*",
       },
+      {
+        source: "/assets/:path*",
+        destination: "https://integr8-634054956771.us-central1.run.app/assets/:path*",
+      },
     ];
   },
 };
