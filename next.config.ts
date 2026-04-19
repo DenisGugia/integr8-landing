f79f5bb42cd26fc7bf5360bfd158b63159978f35
Denis Alves Gugia <denis.gugia@gmail.com>
2026-04-19 16:27:38 -0400

    fix: corrigir sintaxe next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   onDemandEntries: {
     maxInactiveAge: 60 * 1000,
     pagesBufferLength: 5,
   },
-  turbopack: {},
-async rewrites() {
-  return [
-    {
-      source: '/onboarding',
-      destination: 'https://integr8-634054956771.us-central1.run.app',
-    },
-    {
-      source: '/onboarding/:path*',
-      destination: 'https://integr8-634054956771.us-central1.run.app/:path*',
-    },
-  ];
-},
+  async rewrites() {
+    return [
+      {
+        source: "/onboarding",
+        destination: "https://integr8-634054956771.us-central1.run.app",
+      },
+      {
+        source: "/onboarding/:path*",
+        destination: "https://integr8-634054956771.us-central1.run.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
